ae426cacc59687579ac4093019dc362cd383eb4b
Ali Lahrime <alahrime@email.wm.edu>
2019-01-24 12:39:57 -0500

    added functions to Item.js

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,7 +1,24 @@
-'use strict';
-/* global  */
+//'use strict';
+/* global, $, cuid  */
 
 const Item = (function(){
+  validateName: function(name) {
+    if (name === '') {
+      throw TypeError("Name does not exist");
+    }
+  };
+  create: function(name) {
+    return {
+      name: name,
+      id: cuid(),
+      checked: false
+    }
+  }
+
   return {
+    validateName: validateName,
+    create: create
   };
 }());
+
+//Item.create('apples');
